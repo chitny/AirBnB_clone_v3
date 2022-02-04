e3304988765b741f5eaf4739dfc64e3a4f45e957
Marcelo <3338@holbertonschool.com>
2022-02-04 12:42:34 -0800

    cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -42,3 +42,38 @@ def delete_one_city(city_id):
             storage.save()
             return jsonify({}), 200
         abort(404)
+
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
+def create_new_city(state_id):
+    """ Creates a City: POST /api/v1/states/<state_id>/cities """
+    if request.method == 'POST':
+        if storage.get(State, state_id) is not None:
+            req_type = request.headers.get('Content-Type')
+            if req_type != 'application/json':
+                return jsonify('Not a JSON'), 400
+            dict_req_name = request.get_json()
+            if 'name' not in dict_req_name:
+                return jsonify('Missing name'), 400
+            new_obj_City = City(**dict_req_name)
+            new_obj_City.state_id = state_id
+            new_obj_City.save()
+            return jsonify(new_obj_City.to_dict()), 201
+        abort(404)
+
+
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+def update_city(city_id):
+    """ Updates a City object: PUT /api/v1/cities/<city_id> """
+    if request.method == 'PUT':
+        req_type = request.headers.get('Content-Type')
+        if req_type != 'application/json':
+            return jsonify('Not a JSON'), 400
+        dict_req = request.get_json()
+        if storage.get(City, city_id) is not None:
+            if 'name' in dict_req:
+                storage.get(City, city_id).name = dict_req['name']
+                storage.get(City, city_id).save()
+                return jsonify(storage.get(City, city_id).to_dict()), 200
+        abort(404)

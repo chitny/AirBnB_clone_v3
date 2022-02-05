2278adf5a7cafbd133647236943ac44b2a41cc00
Marcelo <3338@holbertonschool.com>
2022-02-05 08:43:56 -0800

    places init

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -43,3 +43,59 @@ def delete_one_place(place_id):
             storage.save()
             return jsonify({}), 200
         abort(404)
+
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
+def create_new_place(city_id):
+    """ Creates a Place: POST /api/v1/cities/<city_id>/places """
+    if request.method == 'POST':
+        if storage.get(City, city_id) is not None:
+            req_type = request.headers.get('Content-Type')
+            if req_type != 'application/json':
+                return jsonify('Not a JSON'), 400
+            dict_req = request.get_json()
+            if 'user_id' not in dict_req:
+                return jsonify('Missing user_id'), 400
+            if storage.get(User, dict_req['user_id']) is None:
+                abort(404)
+            else:
+                if 'name' not in dict_req:
+                    return jsonify('Missing name'), 400
+                new_obj_Place = Place(**dict_req)
+                new_obj_Place.city_id = city_id
+                new_obj_Place.user_id = dict_req['user_id']
+                new_obj_Place.save()
+                return jsonify(new_obj_Place.to_dict()), 201
+        abort(404)
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+def update_place(place_id):
+    """ Updates a Place object: PUT /api/v1/places/<place_id> """
+    if request.method == 'PUT':
+        place = storage.get(Place, place_id)
+        req_type = request.headers.get('Content-Type')
+        if req_type != 'application/json':
+            return jsonify('Not a JSON'), 400
+        dict_req = request.get_json()
+        if place is not None:
+            if 'name' in dict_req:
+                place.name = dict_req['name']
+            if 'description' in dict_req:
+                place.description = dict_req['description']
+            if 'number_rooms' in dict_req:
+                place.number_rooms = dict_req['number_rooms']
+            if 'number_bathrooms' in dict_req:
+                place.number_bathrooms = dict_req['number_bathrooms']
+            if 'max_guest' in dict_req:
+                place.max_guest = dict_req['max_guest']
+            if 'price_by_night' in dict_req:
+                place.price_by_night = dict_req['price_by_night']
+            if 'latitude' in dict_req:
+                place.latitude = dict_req['latitude']
+            if 'longitude' in dict_req:
+                place.longitude = dict_req['longitude']
+            storage.save()
+            return jsonify(place.to_dict()), 200
+        abort(404)
